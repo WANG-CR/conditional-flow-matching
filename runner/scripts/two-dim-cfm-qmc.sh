e4dc64ac2bd52c978459558710d559de968e8378
WANG-CR <dragon.wcr12@gmail.com>
2024-10-15 07:46:21 -0400

    [update] script for ot-cfm

--- runner/scripts/two-dim-cfm-qmc.sh
+++ runner/scripts/two-dim-cfm-qmc.sh
@@ -5,11 +5,11 @@
 
 # twodim is not performed here since we only focus on QMC sampling for gaussian distribution
 python src/train.py -m experiment=cfm \
-  model=cfm,fm \
+  model=cfm,fm,otcfm \
   launcher=mila_cpu_cluster \
   model.sigma_min=0.1 \
   datamodule=scurve,moons,gaussians \
   model.plot=true \
-  model.gaussain_sampler=QMC \
-  seed=42 &
+  model.gaussain_sampler=QMC,rand \
+  seed=43,44,45 &
   # seed=42,43,44,45,46 &
